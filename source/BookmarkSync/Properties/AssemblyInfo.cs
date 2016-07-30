42362bf26ce5963a51358a51cafb2602ebb36609
Gabriel Lemire <gabriel.lemire@gmail.com>
2016-07-29 20:01:02 -0400

    chore: appVeyor versioning configuration
    Update of the AppVeyor configuration for versioning.

--- source/BookmarkSync/Properties/AssemblyInfo.cs
+++ source/BookmarkSync/Properties/AssemblyInfo.cs
@@ -17,3 +17,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("43c1ff41-2ce2-4eae-a5ed-132f2a908740")]
+
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
